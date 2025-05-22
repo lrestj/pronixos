fedcb817495a4a530f82215811036f137c41cce5
libor <rest@seznam.cz>
2025-05-22 18:31:08 +0200

    Install.sh clone path fix

--- .dotfiles/scripts/install.sh
+++ .dotfiles/scripts/install.sh
@@ -6,11 +6,11 @@ alias cfg='git --git-dir=$HOME/.cfg-git/ --work-tree=$HOME'
 
 echo ".cfg.git" >> .gitignore &&
 
-echo "Klonuji GitHub repozitář"
+echo "Kopírování z GitHub repozitáře..."
 
 #git clone --bare ssh://git@codeberg.org/lrestj/nixos.git $HOME/.cfg.git &&
 
-git clone --bare git@github.com:lrestj/nixos.git $HOME/.cfg.git
+git clone --bare git@github.com:lrestj/nixos.git $HOME/.cfg.git &&
 
 alias cfg='git --git-dir=$HOME/.cfg.git/ --work-tree=$HOME'
 
@@ -18,7 +18,7 @@ echo "Přesun konfigurace..."
 
 cfg checkout -f &&
 
-cp -fv /etc/nixos/hardware-configuration.nix /home/libor/.dotfiles/nixos/modules &&
+cp -fv /etc/nixos/hardware-configuration.nix /home/libor/.dotfiles/nixos/ &&
 
 cfg config --local status.showUntrackedFiles no
 
