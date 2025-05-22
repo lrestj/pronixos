cbbe36653100ef9cfc03fd512d5544b6306dd5b2
libor <rest@seznam.cz>
2025-05-22 15:05:44 +0200

    Install.sh edited

--- .dotfiles/scripts/install.sh
+++ .dotfiles/scripts/install.sh
@@ -1,38 +1,25 @@
 #!/bin/sh
 
-#git clone git@github.com:lrestj/nixos.git /home/libor/.dotfiles
-cp /etc/nixos/hardware-configuration.nix /home/libor/.dotfiles/modules &&
-sudo rm -r /etc/nixos &&
-sudo ln -s ~/.dotfiles /etc/nixos &&
+echo "Spouštím instalaci...."
 
-rm -rf /home/libor/.config/clipse
-rm -rf /home/libor/.config/fish
-rm -rf /home/libor/.config/gtk-3.0
-rm -rf /home/libor/.config/gtk-4.0
-rm -rf /home/libor/.config/hypr
-rm -rf /home/libor/.config/mako
-rm -rf /home/libor/.config/rofi
-rm -rf /home/libor/.config/waybar
-rm -rf /home/libor/.config/wlogout
-rm -rf /home/libor/.config/yazi
-rm -rf /home/libor/.config/REAPER
-rm -rf /home/libor/.config/alacritty
-rm -rf /home/libor/.config/kitty
-rm -rf /home/libor/.config/ranger
-ln -s /home/libor/.dotfiles/dotconfig/clipse /home/libor/.config/
-ln -s /home/libor/.dotfiles/dotconfig/fish /home/libor/.config/
-ln -s /home/libor/.dotfiles/dotconfig/gtk-3.0 /home/libor/.config/
-ln -s /home/libor/.dotfiles/dotconfig/gtk-4.0 /home/libor/.config/
-ln -s /home/libor/.dotfiles/dotconfig/hypr /home/libor/.config/
-ln -s /home/libor/.dotfiles/dotconfig/mako /home/libor/.config/
-ln -s /home/libor/.dotfiles/dotconfig/rofi /home/libor/.config/
-ln -s /home/libor/.dotfiles/dotconfig/waybar /home/libor/.config/
-ln -s /home/libor/.dotfiles/dotconfig/wlogout /home/libor/.config/
-ln -s /home/libor/.dotfiles/dotconfig/yazi /home/libor/.config/
-ln -s /home/libor/.dotfiles/dotconfig/REAPER /home/libor/.config/
-ln -s /home/libor/.dotfiles/dotconfig/alacritty /home/libor/.config/
-ln -s /home/libor/.dotfiles/dotconfig/kitty /home/libor/.config/
-ln -s /home/libor/.dotfiles/dotconfig/ranger /home/libor/.config/
+alias cfg='git --git-dir=$HOME/.cfg-git/ --work-tree=$HOME'
 
+echo ".cfg.git" >> .gitignore &&
 
+echo "Klonuji GitHub repozitář"
 
+git clone --bare ssh://git@codeberg.org/lrestj/nixos.git $HOME/.cfg.git &&
+
+#git clone --bare git@github.com:lrestj/nixos.git $HOME/.cfg.git
+
+alias cfg='git --git-dir=$HOME/.cfg.git/ --work-tree=$HOME'
+
+echo "Přesun konfigurace..."
+
+cfg checkout -f &&
+
+cp /etc/nixos/hardware-configuration.nix /home/libor/.dotfiles/nixos/modules &&
+
+#sudo rm -r /etc/nixos
+
+echo "Instalace konfiguračních souborů proběhla úspěšně."
