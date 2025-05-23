08a5b3eff362d9f32b00df26bc78c6aec2808e6c
libor <rest@seznam.cz>
2025-05-23 14:38:23 +0200

    Eof everywhere

--- .dotfiles/nixos/hardware-configuration.nix
+++ .dotfiles/nixos/hardware-configuration.nix
@@ -37,3 +37,6 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
+
+
+#####  END OF FILE  #####
