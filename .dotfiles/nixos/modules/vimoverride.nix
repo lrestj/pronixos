0e9c76eb1844483de7d9bb01ec9b34e11776c9b4
libor <rest@seznam.cz>
2025-05-30 17:28:48 +0200

    Vimoverride clean

--- .dotfiles/nixos/modules/vimoverride.nix
+++ .dotfiles/nixos/modules/vimoverride.nix
@@ -1,23 +1,22 @@
 { config, pkgs, lib, ... }:
 
 {
-   nixpkgs.overlays = [
-     (final: prev:
-       {
-     vim-full = prev.vim-full.overrideAttrs (old: {
-       src = prev.fetchFromGitHub {
-         owner = "vim";
-         repo = "vim";
-         rev = "5199567fd0c9792827287bbd719c69cfd323ecb4";
-         # If you don't know the hash, the first time, set:
-         # hash = "";
-         # then nix will fail the build with such an error message:
-         # hash mismatch in fixed-output derivation '/nix/store/m1ga09c0z1a6n7rj8ky3s31dpgalsn0n-source':
-         # specified: sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=
-         # got:    sha256-173gxk0ymiw94glyjzjizp8bv8g72gwkjhacigd1an09jshdrjb4
-         sha256 = "4Smwlnr5V/q+pmHHMmoalRrvF2MSPRSKZstFTX4YQmA=";
-       };
+  nixpkgs.overlays = [
+    (final: prev: {
+      vim-full = prev.vim-full.overrideAttrs (old: {
+        src = prev.fetchFromGitHub {
+          owner = "vim";
+          repo = "vim";
+          rev = "5199567fd0c9792827287bbd719c69cfd323ecb4";
+          # If you don't know the hash, the first time, set:
+          # hash = "";
+          # then nix will fail the build with such an error message:
+          # hash mismatch in fixed-output derivation '/nix/store/m1ga09c0z1a6n7rj8ky3s31dpgalsn0n-source':
+          # specified: sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=
+          # got:    sha256-173gxk0ymiw94glyjzjizp8bv8g72gwkjhacigd1an09jshdrjb4
+          sha256 = "4Smwlnr5V/q+pmHHMmoalRrvF2MSPRSKZstFTX4YQmA=";
+      };
      });
-       }
-  )];
+    })
+  ];
 }
