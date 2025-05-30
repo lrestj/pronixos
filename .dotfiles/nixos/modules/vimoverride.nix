4d8637102e5214e5a04d2d803257ae5c99768392
libor <rest@seznam.cz>
2025-05-30 19:14:31 +0200

    Vimoverride clean

--- .dotfiles/nixos/modules/vimoverride.nix
+++ .dotfiles/nixos/modules/vimoverride.nix
@@ -14,7 +14,7 @@
           # hash mismatch in fixed-output derivation '/nix/store/m1ga09c0z1a6n7rj8ky3s31dpgalsn0n-source':
           # specified: sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=
           # got:    sha256-173gxk0ymiw94glyjzjizp8bv8g72gwkjhacigd1an09jshdrjb4
-          sha256 = "4Smwlnr5V/q+pmHHMmoalRrvF2MSPRSKZstFTX4YQmA=";
+          hash = "sha256-4Smwlnr5V/q+pmHHMmoalRrvF2MSPRSKZstFTX4YQmA=";
       };
      });
     })
