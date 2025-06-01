5cd2af511fa20e745f682c60d7276d70d72b9bd0
libor <rest@seznam.cz>
2025-06-01 13:48:53 +0200

    Touchpad script added

---
+++ .dotfiles/scripts/asktouchpad.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+
+# Replace {touchpad_device_name} with the actual name of your touchpad
+touchpad_device_name="synps/2-synaptics-touchpad"
+
+#Function to disable touchpad
+disable_touchpad() {
+  hyprctl keyword device[${touchpad_device_name}]:enabled false
+  echo "Touchpad zakázán"
+}
+
+# Function to enable touchpad
+enable_touchpad() {
+  hyprctl keyword device[${touchpad_device_name}]:enabled true
+  echo "Touchpad aktivní"
+}
+
+# Ask user
+echo "Aktivovat/zakázat touchpad [a,z] ???"
+read input
+ if [ $input = "z" ]; then
+  disable_touchpad
+ else
+   enable_touchpad
+ fi
+
+sleep 1 
