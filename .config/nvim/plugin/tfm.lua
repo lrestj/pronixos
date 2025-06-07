9ef0d44b7c8cf16c1e066ea19d259088112e43c8
libor <rest@seznam.cz>
2025-06-07 21:28:40 +0200

    Fix

--- .config/nvim/plugin/tfm.lua
+++ .config/nvim/plugin/tfm.lua
@@ -1,4 +1,12 @@
 require("tfm").setup({
 	replace_netrw = true,
   enable_cmds = true,
-  })
+  -- Customise UI. The below options are the default
+  ui = {
+    border = "rounded",
+    height = 1,
+    width = 0.7,
+    x = 0.2,
+    y = 0.2,
+  },
+})
