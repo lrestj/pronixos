115eaf35d56b0736d4a61c6bfcf152cca145c651
libor <rest@seznam.cz>
2025-06-07 12:29:58 +0200

    Install script edit

--- .dotfiles/scripts/install.sh
+++ .dotfiles/scripts/install.sh
@@ -2,23 +2,19 @@
 
 echo "Spouštím instalaci..."
 
-alias cfg='git --git-dir=$HOME/.cfg-git/ --work-tree=$HOME'
-
 echo ".cfg.git" >> .gitignore &&
 
-echo "Kopírování z CodeBerg repozitáře..."
+echo "kopírování z Codeberg repozitáře..."
 
 git clone --bare https://codeberg.org/lrestj/nixos.git &&
 
-alias cfg='git --git-dir=$HOME/.cfg.git/ --work-tree=$HOME'
-
 echo "Přesun konfigurace..."
 
-cfg checkout -f &&
+git --git-dir=$HOME/.cfg-git/ --work-tree=$HOME checkout -f &&
 
 cp -fv /etc/nixos/hardware-configuration.nix /home/libor/.dotfiles/nixos/ && 
 
-cfg config --local status.showUntrackedFiles no
+git --git-dir=$HOME/.cfg-git/ --work-tree=$HOME config --local status.showUntrackedFiles no
 
 echo "Konfigurace proběhla úspěšně."
 
@@ -27,3 +23,6 @@ ls --color=auto /home/.dotfiles/
 echo "Nyní lze smazat původní adresář nixos."
 
 echo " #sudo rm -r /etc/nixos "
+
+
+##### END OF FILE #####
