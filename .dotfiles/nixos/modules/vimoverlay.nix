24a008f5904ad56f6714b46080112299728ae3ae
libor <rest@seznam.cz>
2025-06-11 21:48:08 +0200

    Vim clipboard plugin fix

--- .dotfiles/nixos/modules/vimoverlay.nix
+++ .dotfiles/nixos/modules/vimoverlay.nix
@@ -10,7 +10,7 @@
           repo = "vim";
           rev = "HEAD";
           # hash = "sha256-28q2FuAI/lUrLQWiGY358+52cRzV3NcO1jyrrgQ21Ik=";
-          hash = "sha256-28q2FuAI/lUrLQWiGY358+52cRzV3NcO1jyrrgQ21Ik=";
+          hash = "sha256-4036xk7lFA02mR/p8nk78VF9H2iRV9IiiQXtUYzuImk=";
         };
      });
     })
