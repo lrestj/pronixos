a8df2214750469e49789cc04706e3e67ce41d3cd
libor <rest@seznam.cz>
2025-06-22 23:38:32 +0200

    Update

--- .dotfiles/nixos/flake.nix
+++ .dotfiles/nixos/flake.nix
@@ -4,19 +4,19 @@
 
   
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable"; 
+      nixpkgs.url = "nixpkgs/nixos-unstable"; 
   };
 
 
   outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations = {
-      nixos = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };
-        system = "x86_64-linux";
-        modules = [ ./configuration.nix
-        ];
+      nixosConfigurations = {
+          nixos = nixpkgs.lib.nixosSystem {
+              specialArgs = { inherit inputs; };
+              system = "x86_64-linux";
+              modules = [ ./configuration.nix
+              ];
+          };
       };
-    };
   }; 
 }
 
--- .dotfiles/nixos/modules/pkgs.nix
+++ .dotfiles/nixos/modules/pkgs.nix
@@ -2,134 +2,131 @@
 
 {
 
- imports =
-    [ 
+  imports = [ 
       ./joypixels.nix
-       ./vimoverlay.nix
-    ];
-
-
- fonts.packages = with pkgs; [
-    font-awesome
-    noto-fonts
-    nerd-fonts.hack
-    jetbrains-mono
-    nerd-fonts.symbols-only
   ];
-
-
+  
+  
+  fonts.packages = with pkgs; [
+      font-awesome
+      noto-fonts
+      nerd-fonts.hack
+      jetbrains-mono
+      nerd-fonts.symbols-only
+  ];
+  
+  
   environment.systemPackages = with pkgs; [
-    kdePackages.ark
-    reaper  
-    reaper-reapack-extension
-    reaper-sws-extension
-    brave
-    brightnessctl
-    btop
-    clipse
-    efibootmgr
-    exfatprogs
-    fastfetch
-    figlet
-    fish
-    foot
-    fzf
-    kdePackages.filelight
-    font-manager
-    geany
-    gimp
-    gthumb
-    hunspellDicts.cs_CZ
-    hunspellDicts.en_US
-    hypridle
-    hyprlock
-    hyprshot
-    jq
-    kdePackages.breeze
-    kdePackages.breeze-icons
-    kdePackages.kalk
-    kdePackages.partitionmanager
-    kdePackages.qt6ct
-    kdePackages.qtsvg
-    kdePackages.qtwayland
-    killall
-    krename
-    libnotify
-    libreoffice-fresh
-    lxqt.lxqt-policykit
-    mako
-    mpv
-    neovim
-    networkmanagerapplet
-    nwg-look
-    kdePackages.okular
-    kdePackages.oxygen-icons
-    pamixer
-    pavucontrol
-    pcmanfm-qt
-    pdfarranger
-    qimgv
-    qjackctl
-    qpwgraph
-    ripgrep
-    rofi rofimoji
-    sddm-chili-theme
-    simple-scan
-    slurp
-    swaybg
-    udiskie
-    ueberzugpp
-    unzip
-    vim-full
-    vlc
-    waybar
-    waypaper
-    wget
-    wl-clipboard
-    wlogout
-    wlsunset
-    xdg-desktop-portal-gtk
-    xdg-desktop-portal-hyprland
-    xdg-utils
-    xournalpp
-    yazi
-    ytdownloader
-    zip p7zip
-    (pkgs.writeScriptBin "sct" ''
-      #!/bin/sh
-      killall wlsunset &> /dev/null;
-      if [ $# -eq 1 ]; then
-        temphigh=$(( $1 + 1 ))
-        templow=$1
-        wlsunset -t $templow -T $temphigh &> /dev/null &
-      else
-        killall wlsunset &> /dev/null;
-      fi
-    '')
+      kdePackages.ark
+      reaper  
+      reaper-reapack-extension
+      reaper-sws-extension
+      brave
+      brightnessctl
+      btop
+      clipse
+      efibootmgr
+      exfatprogs
+      fastfetch
+      figlet
+      fish
+      foot
+      fzf
+      kdePackages.filelight
+      font-manager
+      geany
+      gimp
+      gthumb
+      hunspellDicts.cs_CZ
+      hunspellDicts.en_US
+      hypridle
+      hyprlock
+      hyprshot
+      jq
+      kdePackages.breeze
+      kdePackages.breeze-icons
+      kdePackages.kalk
+      kdePackages.partitionmanager
+      kdePackages.qt6ct
+      kdePackages.qtsvg
+      kdePackages.qtwayland
+      killall
+      krename
+      libnotify
+      libreoffice-fresh
+      lxqt.lxqt-policykit
+      mako
+      mpv
+      neovim
+      networkmanagerapplet
+      nwg-look
+      kdePackages.okular
+      kdePackages.oxygen-icons
+      pamixer
+      pavucontrol
+      pcmanfm-qt
+      pdfarranger
+      qimgv
+      qjackctl
+      qpwgraph
+      ripgrep
+      rofi rofimoji
+      sddm-chili-theme
+      simple-scan
+      slurp
+      swaybg
+      udiskie
+      ueberzugpp
+      unzip
+      vim-full
+      vlc
+      waybar
+      waypaper
+      wget
+      wl-clipboard
+      wlogout
+      wlsunset
+      xdg-desktop-portal-gtk
+      xdg-desktop-portal-hyprland
+      xdg-utils
+      xournalpp
+      yazi
+      ytdownloader
+      zip p7zip
+      (pkgs.writeScriptBin "sct" ''
+          #!/bin/sh
+          killall wlsunset &> /dev/null;
+          if [ $# -eq 1 ]; then
+            temphigh=$(( $1 + 1 ))
+            templow=$1
+            wlsunset -t $templow -T $temphigh &> /dev/null &
+          else
+            killall wlsunset &> /dev/null;
+          fi
+      '')
   ];
-
-
+  
+  
   programs = {
-    hyprland = {
-        enable = true;
-        xwayland.enable = true;
+      hyprland = {
+          enable = true;
+          xwayland.enable = true;
       };
-    dconf.enable = true;  
-    kdeconnect.enable = true;
-    git = {
-      enable = true;
-      config = {
-        safe.directory = "/home/libor/.dotfiles";
-        init = {
-          defaultBranch = "main";
-          userName  = "libor";
-          userEmail = "rest@seznam.cz";
-        };
+      dconf.enable = true;  
+      kdeconnect.enable = true;
+      git = {
+          enable = true;
+          config = {
+              safe.directory = "/home/libor/.dotfiles";
+              init = {
+                  defaultBranch = "main";
+                  userName  = "libor";
+                  userEmail = "rest@seznam.cz";
+              };
+          };
       };
-    };
   };
 
 }
-
 
 #####  END OF FILE  #####
