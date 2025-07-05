7aa2486db17ba8555f7649b5ef5356a60964dc86
LrestJ <rest@seznam.cz>
2025-07-05 15:24:46 +0200

    Nfs lazy mounting added

--- .dotfiles/nixos/configuration.nix
+++ .dotfiles/nixos/configuration.nix
@@ -164,17 +164,17 @@
   fileSystems."/data/nfs/FilmyNas" = {
       device = "192.168.77.18:/volume1/Filmy";
       fsType = "nfs";
-      options = [ "nfsvers=4" "users" "nofail"  ];
+      options = [ "nfsvers=4" "x-systemd.automount" "noauto" "x-systemd.iddle-timeout=450" "nofail" ];
   };
   fileSystems."/data/nfs/DataNas" = {
       device = "192.168.77.18:/volume1/Rodinas";
       fsType = "nfs";
-      options = [ "nfsvers=4" "users" "nofail" ];
+      options = [ "nfsvers=4" "x-systemd.automount" "noauto" "x-systemd.iddle-timeout=450" "nofail" ];
   };
   fileSystems."/data/nfs/HudbaNas" = {
       device = "192.168.77.18:/volume1/Hudba";
       fsType = "nfs";
-      options = [ "nfsvers=4" "users" "nofail" ];
+      options = [ "nfsvers=4" "x-systemd.automount" "noauto" "x-systemd.iddle-timeout=450" "nofail" ];
   };
   
   # Release version of the first install of this system
