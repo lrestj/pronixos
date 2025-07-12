79a98a6a9848a32d2678aee9aa3aadd4e6cddd68
LrestJ <rest@seznam.cz>
2025-07-12 14:48:18 +0200

    Flake.nix back to normal

--- .dotfiles/nixos/flake.nix
+++ .dotfiles/nixos/flake.nix
@@ -13,16 +13,6 @@
     let
       system = "x86_64-linux";
       lib = nixpkgs.lib;
-
-      # Systems that can run tests:
-      supportedSystems = [ "aarch64-linux" "i686-linux" "x86_64-linux" ];
-
-      # Function to generate a set based on supported systems:
-      forAllSystems = inputs.nixpkgs.lib.genAttrs supportedSystems;
-
-      # Attribute set of nixpkgs for each system:
-      nixpkgsFor =
-        forAllSystems (system: import inputs.nixpkgs { inherit system; });
     in
   {
       nixosConfigurations = {
@@ -37,28 +27,6 @@
               ];
           };
       };
-      packages = forAllSystems (system:
-          let pkgs = nixpkgsFor.${system};
-          in {
-              default = self.packages.${system}.install;
-
-              install = pkgs.writeShellApplication {
-                  name = "install";
-                  runtimeInputs = with pkgs; [ git ]; # deps
-                  text = ''${./datetimenotify.sh} "$@"''; # the script
-              };
-
-      });
-
-      apps = forAllSystems (system: {
-          default = self.apps.${system}.install;
-
-          install = {
-            type = "app";
-            program = "${self.packages.${system}.install}/bin/install";
-          };
-      });      
-
   }; 
 }
 
