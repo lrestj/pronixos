71fa37c77ad7cf6469401cfcdaa27faba499ec1d
LrestJ <rest@seznam.cz>
2025-07-19 14:42:03 +0200

    Console font declared

--- .dotfiles/nixos/modules/console.nix
+++ .dotfiles/nixos/modules/console.nix
@@ -5,9 +5,11 @@
   console.enable = false;
   services.kmscon = {
       enable = true;
+      autologinUser = "libor";
       fonts = [ { name = "Hack Nerd Font"; package = pkgs.nerd-fonts.hack; } ];
       hwRender = true;
       useXkbConfig = true;
+      extraConfig = "font-size=12";
   };
 
 }
