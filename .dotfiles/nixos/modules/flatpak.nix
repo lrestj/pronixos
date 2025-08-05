bc71e1d97a9ccd9926ec0b51ed1f94fb38ac33fd
LrestJ <rest@seznam.cz>
2025-08-05 14:01:12 +0200

    Flatpak module ready

---
+++ .dotfiles/nixos/modules/flatpak.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, lib, ... }:
+
+{
+    services.flatpak = {
+        enable = true;
+        packages = [
+            "org.vim.Vim"
+        ];
+        update.auto = {
+            enable = true;
+            onCalendar = "weekly";
+        };
+    };
+}
+
+#####  END OF FILE  #####
