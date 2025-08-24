de76492f33e96b216e8bb461060b083256cfac3d
LrestJ <rest@seznam.cz>
2025-08-24 22:52:38 +0200

    Hyprland input latest back

--- .dotfiles/nixos/modules/pkgs.nix
+++ .dotfiles/nixos/modules/pkgs.nix
@@ -133,8 +133,8 @@
       hyprland = {
            enable = true;
            xwayland.enable = true;
-          # package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-          # portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+           package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+           portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
       };
       git = {
           enable = true;
