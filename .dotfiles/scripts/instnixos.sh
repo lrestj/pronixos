a8b02402a11a69b2eeff9bdca9f223268c77721c
LrestJ <rest@seznam.cz>
2025-08-26 20:38:28 +0200

    Fixes

--- .dotfiles/scripts/instnixos.sh
+++ .dotfiles/scripts/instnixos.sh
@@ -6,7 +6,7 @@ echo ".cfg.git" >> .gitignore &&
 
 tput setaf 166 bold; echo "Kopírování z repozitáře..."
 
-git clone --bare https://github.com/lrestj/nixos.git /home/libor/.cfg.git
+git clone --bare https://github.com/lrestj/pronixos.git /home/libor/.cfg.git
 
 tput setaf 166 bold; echo "Přesun konfigurace..."
 
@@ -25,8 +25,7 @@ echo " #sudo rm -r /etc/nixos "
 git config --global user.email "rest@seznam.cz"
 git config --global user.name "LrestJ"
 git --git-dir=/home/libor/.cfg.git/ --work-tree=/home/libor remote remove origin
-git --git-dir=/home/libor/.cfg.git/ --work-tree=/home/libor remote add github git@github.com:lrestj/nixos.git
-git --git-dir=/home/libor/.cfg.git/ --work-tree=/home/libor remote add gitlab git@gitlab.com:lrestj/nixos.git
+git --git-dir=/home/libor/.cfg.git/ --work-tree=/home/libor remote add github git@github.com:lrestj/pronixos.git
 git --git-dir=/home/libor/.cfg.git/ --work-tree=/home/libor remote add codeberg ssh://git@codeberg.org/lrestj/nixos.git
 
 echo -e "\n"
